680304b9a55871c7bb27e7a80594cf470be4e742
antoine sierz <sierzputowskianotine@gmail.com>
2022-12-03 17:03:00 +0100

    first commit

---
+++ count.js
@@ -0,0 +1,24 @@
+import fetch from "node-fetch";
+const script_url = "https://www.fnac.es/static-proxy/kameleoon/script.js";
+
+const getScript = () => {
+  return fetch(script_url)
+    .then(function (res) {
+      if (res.ok) {
+        return res.text();
+      }
+    })
+    .then(function (value) {
+      return value;
+    });
+};
+
+const getValues = (text) => {
+  let count = text.match(/tc_vars/g).length;
+  return count;
+};
+
+getScript().then((value) => {
+  console.log(getValues(value));
+  return getValues(value);
+});
